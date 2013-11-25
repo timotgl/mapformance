f23407b42087072d8006e7b0abfc29d5f27be375
Timo Taglieber <mail@timotaglieber.de>
2013-11-25 22:19:14 +0100

    express serves index.html as static file when the root url is requested

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var express = require('express'),
+    app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.use(express.logger());
+app.use(express.static(__dirname));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
