9a4c80cdc099331ebe147620b42ff6a77ca19248
Timo Taglieber <mail@timotaglieber.de>
2013-11-27 01:07:44 +0100

    Removed angular-ui-map, added sharedService to communicate between controllers

--- app/controllers/AddressController.js
+++ app/controllers/AddressController.js
@@ -1,8 +1,9 @@
-mapformanceApp.controller('AddressController', function ($scope) {
+mapformanceApp.controller('AddressController', function ($scope, addMarkerService) {
     $scope.names = ['Timo', 'Bino'];
     this.queryDefault = 'Rigaer Str. 4, 10247 Berlin';
     this.query = this.queryDefault;
     this.searchForAddress = function () {
         console.log('Searching addresses for query:', this.query);
+        addMarkerService.addMarker(52.518108, 13.454538, this.query);
     };
 });
--- app/controllers/MapController.js
+++ app/controllers/MapController.js
@@ -1,7 +1,26 @@
-mapformanceApp.controller('MapController', function ($scope) {
-    $scope.mapOptions = {
-        center : new google.maps.LatLng(52.518108, 13.454538),
-        zoom : 15,
-        mapTypeId : google.maps.MapTypeId.ROADMAP
-    };
+mapformanceApp.controller('MapController', function ($scope, addMarkerService) {
+    google.maps.visualRefresh = true;
+    this.map = new google.maps.Map(
+        document.getElementById('googleMap'),
+        {
+            zoom: 15,
+            center: new google.maps.LatLng(52.518108, 13.454538),
+            mapTypeId : google.maps.MapTypeId.ROADMAP
+        }
+    );
+    $scope.$on('markerAdded', function() {
+        console.log('query=', addMarkerService.query);
+        console.log('lat=', addMarkerService.lat);
+        console.log('long=', addMarkerService.long);
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(
+                addMarkerService.lat,
+                addMarkerService.long
+            ),
+            title:"Hello World!"
+        });
+        
+        // TODO: $scope.map and this.map don't work to access the Map instance
+        marker.setMap($scope.map);
+    });
 });
