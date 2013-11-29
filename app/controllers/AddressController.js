abdaba9cb9c0a9c791026948eeeaddeb0718b37d
Timo Taglieber <mail@timotaglieber.de>
2013-11-29 16:47:35 +0100

    Allowing address search by pressing enter in the text input

--- app/controllers/AddressController.js
+++ app/controllers/AddressController.js
@@ -1,8 +1,14 @@
 mapformanceApp.controller('AddressController', function ($scope, geoCoderService) {
     this.query = 'Rigaer Str. 4, 10247 Berlin';
-    this.addMarkerForAddress = function () {
-        console.log('Searching addresses for query:', this.query);
-        geoCoderService.getFirstResultForAddress(this.query);
+    
+    /**
+     * Ask the geocoder to get the first lat/long result for the address and add a marker to the map.
+     * Triggered by pressing ENTER in the text input or clicking the 'Add marker' button.
+     */
+    this.addMarkerForAddress = function (event) {
+        if (event === undefined || event.which === 13) {
+            geoCoderService.getFirstResultForAddress(this.query);            
+        }
     };
 });
 console.log('AddressController loaded');
